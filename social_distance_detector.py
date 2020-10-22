beaa7d95e2aa2fbe8ff7ab3e9e94820fcda2004c
ole thomas <norbye93@gmail.com>
2020-10-22 21:54:05 +0200

    rednet

--- social_distance_detector.py
+++ social_distance_detector.py
@@ -32,7 +32,7 @@ configPath = os.path.sep.join([config.MODEL_PATH, "yolov3.cfg"])
 
 # load our YOLO object detector trained on COCO dataset (80 classes)
 print("[INFO] loading YOLO from disk...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+net = cv2.dnn.readNet(configPath, weightsPath)
 
 # check if we are going to use GPU
 if config.USE_GPU:
