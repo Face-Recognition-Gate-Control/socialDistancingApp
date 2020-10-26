767657d9ff34c0cc7423496977a229947ccb63da
ole thomas <norbye93@gmail.com>
2020-10-26 19:10:44 +0100

    fd

--- pyqt/classes/classes.py
+++ pyqt/classes/classes.py
@@ -360,7 +360,7 @@ class detectionThread(QThread):
                     detections = net.Detect(rgb_img)
                     display.Render(rgb_img)
 
-                    print(detections)
+                    print(detections.ClassID)
 
                     # results = detect_people(blob, net)
                     # predicted_data.put(detections)
