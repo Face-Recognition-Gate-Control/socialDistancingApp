931d78a10067c9b1d9fa3d72f31be380308e5eb5
ole thomas <norbye93@gmail.com>
2020-10-28 12:07:22 +0100

    jk

--- test.py
+++ test.py
@@ -1,22 +1,9 @@
 import jetson.inference
 import jetson.utils
 import pyrealsense2 as rs
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
-display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
-jsonObj = json.load(open("configrealsense.json"))
-json_string = str(jsonObj).replace("'", '"')
 
-pipeline = rs.pipeline()
-rsconfig = rs.config()
-cfg = pipeline.start(rsconfig)
-dev = cfg.get_device()
-advnc_mode = rs.rs400_advanced_mode(dev)
-advnc_mode.load_json(json_string)
 
-# get depthscale from camera. converting distance to meter
-depth_scale = cfg.get_device().first_depth_sensor().get_depth_scale()
 
 def rgbtocuda(image):
 
@@ -28,11 +15,97 @@ def rgbtocuda(image):
 
     return rgb_img
 
-while display.IsStreaming():
 
-    frames = pipeline.wait_for_frames()
-    color_frame = frames.get_color_frame()
-	img = rgbtocuda(color_frame)
-	detections = net.Detect(img)
-	display.Render(img)
-	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
+def test():
+
+    while True:
+
+        try:
+
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            depth_frame = frames.get_depth_frame()
+
+            if not depth_frame or not color_frame:
+                continue
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            img = rgbtocuda(color_frame)
+            detections = net.Detect(img)
+            display.Render(img)
+            display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
+
+
+
+def startApp():
+
+    
+    camera =False
+    # tries to connect to realsense camera
+
+    while not camera:
+        try:
+
+            print("connecting to realsense")
+            cfg = pipeline.start(rsconfig)
+            dev = cfg.get_device()
+            advnc_mode = rs.rs400_advanced_mode(dev)
+            advnc_mode.load_json(json_string)
+
+           
+
+           
+
+           
+
+        except Exception as e:
+            print(e)
+            print("no device connected")
+
+        finally:
+            print("connected")
+            camera = True
+            test()
+
+         
+
+           
+
+
+# check to see if this is the main thread of execution
+if __name__ == '__main__':
+
+    net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+    camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
+    display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
+
+     # load config file made
+    # do adjustment in realsense depth quality tool
+    jsonObj = json.load(open("configrealsense.json"))
+    json_string = str(jsonObj).replace("'", '"')
+
+    pipeline = rs.pipeline()
+    rsconfig = rs.config()
+
+    freq = int(jsonObj["stream-fps"])
+    print("W: ", int(jsonObj["stream-width"]))
+    print("H: ", int(jsonObj["stream-height"]))
+    print("FPS: ", int(jsonObj["stream-fps"]))
+    rsconfig.enable_stream(
+        rs.stream.depth,
+        int(jsonObj["stream-width"]),
+        int(jsonObj["stream-height"]),
+        rs.format.z16,
+        int(jsonObj["stream-fps"]),
+    )
+    rsconfig.enable_stream(
+        rs.stream.color,
+        int(jsonObj["stream-width"]),
+        int(jsonObj["stream-height"]),
+        rs.format.bgr8,
+        int(jsonObj["stream-fps"]),
+    )
+
+    startApp()
