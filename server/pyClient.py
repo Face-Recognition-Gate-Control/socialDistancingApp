7c60bc36d4ae5a59b698274e744f2686a9810dc4
ole thomas <norbye93@gmail.com>
2020-11-06 19:13:10 +0100

    fe

--- server/pyClient.py
+++ server/pyClient.py
@@ -1,32 +1,26 @@
-from threading import Thread
+import threading
 import socket
 import time
 import pickle
 
 
-class ClientPy(Thread):
-
-    def __init__(self,HOST,PORT,sendQueue):
-        Thread.__init__(self)
+class ClientPy(threading.Thread):
+    def __init__(self, HOST, PORT, sendQueue):
+        threading.Thread.__init__(self)
         self.HOST = HOST
         self.PORT = PORT
         self.queue = sendQueue
 
-    
-
-
     def run(self):
-        
-        
+
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((HOST,PORT))
+        s.connect((HOST, PORT))
 
         while True:
-	        data = self.queue.get()
-            data = pickle.dumps(data)
-            s.sendall(pickle.dumps(1))
-	        s.sendall(data)
-            time.sleep(0.1)
+            if not self.queue.empty():
+                data = self.queue.get()
+                data = pickle.dumps(data)
+                s.sendall(pickle.dumps(1))
+                s.sendall(data)
+                time.sleep(0.1)
         s.close()
-        
-
