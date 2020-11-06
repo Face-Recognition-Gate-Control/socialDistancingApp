418e3987c404e5e141c928e0cc2a3b53c2488282
ole thomas <norbye93@gmail.com>
2020-11-06 21:13:53 +0100

    fe

--- utils/post_process.py
+++ utils/post_process.py
@@ -189,7 +189,7 @@ def euclideanDistance(points, min_dist, backup_points, image, queue):
                     + (points[i]["z"] - points[j]["z"]) ** 2
                 )
 
-                queue.put(({"x": math.floor(backup_points[i][0]), "y": math.floor(backup_points[i][1]}))
+                queue.put(({"x": math.floor(backup_points[i][0]), "y": math.floor(backup_points[i][1]})))
 
             image = drawLine(
                 image,
