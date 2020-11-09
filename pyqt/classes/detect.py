2682cc0a15b51f1b8a642c61cd6b644efd4daaa5
ole thomas <norbye93@gmail.com>
2020-11-09 13:54:56 +0100

    fe

--- pyqt/classes/detect.py
+++ pyqt/classes/detect.py
@@ -160,15 +160,22 @@ class Detect:
 
         # automatically determine the size of the blurring kernel based
         # on the spatial dimensions of the input image
-        (h, w) = image.shape[:2]
-        kW = int(w / factor)
-        kH = int(h / factor)
-        # ensure the width of the kernel is odd
-        if kW % 2 == 0:
-            kW -= 1
-        # ensure the height of the kernel is odd
-        if kH % 2 == 0:
-            kH -= 1
-        # apply a Gaussian blur to the input image using our computed
-        # kernel size
-        return cv2.GaussianBlur(image, (kW, kH), 0)
+        try:
+            (h, w) = image.shape[:2]
+            kW = int(w / factor)
+            kH = int(h / factor)
+            # ensure the width of the kernel is odd
+            if kW % 2 == 0:
+                kW -= 1
+            # ensure the height of the kernel is odd
+            if kH % 2 == 0:
+                kH -= 1
+            # apply a Gaussian blur to the input image using our computed
+            # kernel size
+        except Exception as e:
+            print(e)
+
+        else:
+
+            return cv2.GaussianBlur(image, (kW, kH), 0)
+        return image
