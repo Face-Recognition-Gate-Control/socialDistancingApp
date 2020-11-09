8e9017acadd472493c004198cd670d63593adde4
ole thomas <norbye93@gmail.com>
2020-11-09 13:46:33 +0100

    s

--- core/detection/face_recognizer.py
+++ core/detection/face_recognizer.py
@@ -19,7 +19,7 @@ class FaceRecognizer:
 
     INPUT_IMAGE_SIZE = 640  # default cv2-size
     NUMB_OF_RESULTS = 20  # keep 'x' results
-    PROB_THRESHOLD = 0.6  # sens 0 - 1 (1=highest)
+    PROB_THRESHOLD = 0.8  # sens 0 - 1 (1=highest)
     # only consider the candidates with the highest scores.
     CANDIDATE_SIZE = 200
 
