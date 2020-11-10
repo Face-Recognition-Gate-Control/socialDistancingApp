d35b48c76128d083eeaf711bdf1bd7663b2cf7e8
ole thomas <norbye93@gmail.com>
2020-11-10 20:07:33 +0100

    gr

--- pyqt/classes/detect.py
+++ pyqt/classes/detect.py
@@ -106,7 +106,7 @@ class Detect:
         if len(faces_boxes) > 0:
 
             faces = np.array(faces_boxes, dtype="float32")
-            preds = maskNet.predict(faces, batch_size=32)
+            preds = self.maskNet.predict(faces, batch_size=32)
             print(preds)
 
             for facebox in faces_boxes:
