b82a009e97bd5f4563bad2f2e72c6e891516a96e
ole thomas <norbye93@gmail.com>
2020-11-10 21:04:01 +0100

    fe

--- pyqt/classes/detect.py
+++ pyqt/classes/detect.py
@@ -111,7 +111,7 @@ class Detect:
                 (dsx, dsy, dex, dey) = facebox
                 face = color_image[int(dsy) : int((dey)), int(dsx) : int((dex))]
                 face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                # face = cv2.resize(face, (224, 224))
+                face = cv2.resize(face, (224, 224))
                 face = img_to_array(face)
                 face = preprocess_input(face)
                 faces.append(face)
